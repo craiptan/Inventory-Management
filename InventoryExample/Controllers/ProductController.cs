6029d24fdee2fba502979b7f68e4aead4b5f3b8c
christopher githui <christopher.githui@impaxafrica.com>
2022-02-05 10:24:24 +0300

    Add project files.

---
+++ InventoryExample/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost("create")]
+        public IActionResult CreateController()
+        {
+            try
+            {
+                return Ok();
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("AllProducts")]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                return Ok();
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("GetProduct/{id}")]
+        public IActionResult GetProducts(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
